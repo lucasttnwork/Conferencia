a122f3eea48985b4c99b5bbe1341a20e58c901b6
Lucas TTN <lucasttnwork@users.noreply.github.com>
2025-08-14 11:54:52 -0300

    fix(runtime): remove merge markers in server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require('dotenv').config();
 const http = require('http');
 const next = require('next');
@@ -25,35 +24,3 @@ app
     console.error('Error starting Next.js server', err);
     process.exit(1);
   });
-
-
-=======
-require('dotenv').config();
-const http = require('http');
-const next = require('next');
-
-const dev = process.env.NODE_ENV !== 'production';
-const hostname = '0.0.0.0';
-const port = Number(process.env.PORT) || 3000;
-
-const app = next({ dev, hostname, port });
-const handle = app.getRequestHandler();
-
-app
-  .prepare()
-  .then(() => {
-    const server = http.createServer((req, res) => {
-      handle(req, res);
-    });
-
-    server.listen(port, hostname, () => {
-      console.log(`> Ready on http://${hostname}:${port} (dev=${dev})`);
-    });
-  })
-  .catch((err) => {
-    console.error('Error starting Next.js server', err);
-    process.exit(1);
-  });
-
-
->>>>>>> 9faaeadc2d8cb04ceb8537caaf5c55b02cd64a6a
